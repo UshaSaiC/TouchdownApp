abcfe8601399501683ebde529542724adf8ecf67
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 04:29:47 +0530

    Initial Commit

---
+++ TouchdownApp/TouchdownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownAppApp.swift
+//  TouchdownApp
+//
+//  Created by Usha Sai Chintha on 19/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
