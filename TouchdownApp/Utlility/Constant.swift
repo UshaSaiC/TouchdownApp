3cc5e165aa7bde8d172585a571d6bf1b38648cc5
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 05:25:32 +0530

    Added asset files and new groups, files to project

---
+++ TouchdownApp/Utlility/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  TouchdownApp
+//
+//  Created by Usha Sai Chintha on 19/09/22.
+//
+
+import SwiftUI
+
+let colorBackground: Color = Color("ColorBackground")
+let colorGray: Color = Color(UIColor.systemGray4)
