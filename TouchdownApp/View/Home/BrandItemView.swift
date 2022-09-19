64a943c8b6efdd793ee8ba5a2d13536ee43f6891
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 12:36:03 +0530

    Added horizontal grid of logos

---
+++ TouchdownApp/View/Home/BrandItemView.swift
@@ -0,0 +1,32 @@
+//
+//  BrandItemView.swift
+//  TouchdownApp
+//
+//  Created by Usha Sai Chintha on 19/09/22.
+//
+
+import SwiftUI
+
+struct BrandItemView: View {
+    
+    let brand: Brand
+    
+    var body: some View {
+        Image(brand.image)
+            .resizable()
+            .scaledToFit()
+            .padding()
+            .background(Color.white
+                .cornerRadius(12)
+            )
+            .background(RoundedRectangle(cornerRadius: 12)
+                .stroke(Color.gray, lineWidth: 1)
+            )
+    }
+}
+
+struct BrandItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        BrandItemView(brand: brands[0])
+    }
+}
