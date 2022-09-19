cb59f471ddc7016ced95fdfef348928631985150
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-19 11:11:10 +0530

    Added scroll view to display image view on main activity home screen

---
+++ TouchdownApp/Extension/CodableBundleExtention.swift
@@ -0,0 +1,29 @@
+//
+//  CodableBundleExtention.swift
+//  TouchdownApp
+//
+//  Created by Usha Sai Chintha on 19/09/22.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Codable>(_ file: String) -> T {
+       
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let decodedData = try? decoder.decode(T.self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle")
+        }
+        
+        return decodedData
+    }
+}
